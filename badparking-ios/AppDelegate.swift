ed268f8af96763eff2017b4f6a8229640cf09b06
enagorny <ideviantik@gmail.com>
2016-08-31 15:24:55 +0300

    facebook works again

--- badparking-ios/AppDelegate.swift
+++ badparking-ios/AppDelegate.swift
@@ -21,8 +21,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-        return FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: "", annotation: nil)
-      //  return true
+        return FBSDKApplicationDelegate.sharedInstance().application(app,
+                                                                     open: url,
+                                                                     sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!,
+                                                                     annotation: options[UIApplicationOpenURLOptionsKey.annotation])
     }
 
 //  func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: AnyObject) -> Bool {
