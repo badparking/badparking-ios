513241bd91ae12a60a7b9b90939a05587a90f844
Roman Simenok <roma_simenok@mail.ru>
2016-10-21 13:58:19 +0300

    hide API from public

--- BadParking/AppDelegate.swift
+++ BadParking/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        GMSServices.provideAPIKey("AIzaSyCAJfrS8mnuyFwvidCnKDy2C5CtCAULAuw")
+        GMSServices.provideAPIKey(Constants.GMAPIKey)
         
         return true
     }
